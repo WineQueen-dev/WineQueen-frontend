ddad5c5e0c962272e0407617da1e63ca2b267ac3
JunSeoChoi <evan7484@gmail.com>
2025-08-13 14:47:16 +0900

    Fix: 배포 시에도 스트리밍 되도록 보안 연결 변경

--- src/pages/OpenWine.tsx
+++ src/pages/OpenWine.tsx
@@ -2,23 +2,17 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import styles from "../styles/Wine.module.css";
 import chevron from "../assets/chevron.svg";
+import { getWebSocketUrl, getHttpUrl, NetWorkIp } from "../constants/constants";
 
 const OpenWine = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // 쿼리 파라미터 추출
   const queryParams = new URLSearchParams(location.search);
   const wineNumber = queryParams.get("wine") || "0";
 
   useEffect(() => {
-    // HTTPS면 wss://, HTTP면 ws://
-    const wsProtocol =
-      window.location.protocol === "https:" ? "wss://" : "ws://";
-    // host는 현재 접속 도메인 (Vercel 배포시 프록시를 통해 연결)
-    const wsUrl = `${wsProtocol}${window.location.host}/api/ws`;
-
-    const socket = new WebSocket(wsUrl);
+    const socket = new WebSocket(getWebSocketUrl("/ws"));
 
     const pingInterval = setInterval(() => {
       if (socket.readyState === WebSocket.OPEN) {
@@ -27,7 +21,7 @@ const OpenWine = () => {
     }, 5000);
 
     socket.onopen = () => {
-      console.log("✅ WebSocket 연결됨:", wsUrl);
+      console.log("✅ WebSocket 연결됨:", getWebSocketUrl("/ws"));
     };
 
     socket.onmessage = (event) => {
@@ -59,7 +53,7 @@ const OpenWine = () => {
       <div className={styles.section}>
         <div className={styles.rectangle}>
           <img
-            src={`${window.location.protocol}//${window.location.host}/api/video_feed`}
+            src={getHttpUrl("/video_feed")}
             alt="Yolo Stream"
             className={styles.rectangle_img}
             crossOrigin="anonymous"
