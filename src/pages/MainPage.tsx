f046b0439a9917a620bb028967d8e837d7720262
JunSeoChoi <evan7484@gmail.com>
2025-08-29 16:32:58 +0900

    Fix: 요일 영어로 렌더링되게 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -9,7 +9,7 @@ const formatNow = (d: Date) => {
   const y = d.getFullYear();
   const m = String(d.getMonth() + 1).padStart(2, "0");
   const day = String(d.getDate()).padStart(2, "0");
-  const weekday = d.toLocaleDateString(undefined, { weekday: "short" }); // ex) Mon / 월
+  const weekday = d.toLocaleDateString("en-US", { weekday: "short" }); // ex) Mon / 월
   const hh = String(d.getHours()).padStart(2, "0");
   const mm = String(d.getMinutes()).padStart(2, "0");
   const ss = String(d.getSeconds()).padStart(2, "0");
